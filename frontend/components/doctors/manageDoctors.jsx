fcf62ff1573931294036775214d41beb734a4c92
TONY OCHIENG <tonyopuodho@gmail.com>
2025-07-23 10:09:14 +0300

    manage doctors implementation

--- frontend/components/doctors/manageDoctors.jsx
+++ frontend/components/doctors/manageDoctors.jsx
@@ -1,7 +1,32 @@
 
 const ManageDoctors = () => {
     return(
-        <h3>Doctors</h3>
+        <div className="p-6">
+            <div className="p-4 shadow-sm mt-2 cardAp rounded-sm">
+                    <table className="w-full">
+                        <thead className="text-center">
+                            <th>image</th>
+                            <th>first name</th>
+                            <th>last name</th>
+                            <th>phone</th>
+                            <th>specialty</th>
+                            <th>schedule</th>
+                            <th>action</th>
+                        </thead>
+                        <tbody>
+                            <tr className="text-center font-bold ">
+                            <td className="p-2">0</td>
+                            <td>Tony</td>
+                            <td>Ochieng</td>
+                            <td>0794498656</td>
+                            <td>pediatrics</td>
+                            <td>monday</td>
+                            <td>0</td>
+                            </tr>
+                        </tbody>
+                    </table>                
+            </div>
+        </div>
     )
 }
 
