c71c74caf2a9798349a1407ee6621d98e60db08b
TONY OCHIENG <tonyopuodho@gmail.com>
2025-07-28 10:59:01 +0300

    displayig patient details from backend to frontend

--- frontend/components/profile/patientProfile.jsx
+++ frontend/components/profile/patientProfile.jsx
@@ -1,22 +1,35 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 
-const PatientProfile = () => {
-    const [id,setUsername] = useState('')
-    const [userProfile,setUserProfile] = useState([])
-    const name = id
 
+const PatientProfile = () => {
+    const [userProfile,setUserProfile] = useState([])
+    const [patientValue, setPatientValue] = useState([])    
+    
     axios.defaults.withCredentials = true
     useEffect(() => {
         axios.get("http://localhost:3000/auth/patientName")
         .then(result => {
-            console.log(result)
             if (result.data.valid) {
-                setUsername(result.data.username)
+                const patientId = result.data.patientId
+                axios.get(`http://localhost:3000/auth/patientDetail/${patientId}`)
+                .then(result => {
+                    if(result.data.status) {
+                        setUserProfile(result.data.Result)
+                    }
+                })
+                .catch(error => console.log(error))                
             }
          })
         .catch(error => console.log(error))
     },[])
+    
+    const firstName =  userProfile.map((items) => items.firstName)
+    const lastName = userProfile.map((items) => items.lastName)
+    const email = userProfile.map((items) => items.email)
+    const phone = userProfile.map((items) => items.phone)
+    const address = userProfile.map((items) => items.address)
+    const date = userProfile.map(items => items.dateOfbirth)
 
     return(
         <div className="p-4">
@@ -25,19 +38,19 @@ const PatientProfile = () => {
                   <div className="flex gap-8">
                         <div>
                         <label className="font-bold">First name</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>
+                        <input type="text" value={firstName}  onChange={(event) => setPatientValue({...patientValue, firstName:event.target.value})} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>
                         <label className="font-bold">Email</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/> 
+                        <input type="text" value={email} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/> 
                          <label className="font-bold">Address</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>                       
+                        <input type="text" value={address} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>                       
                     </div>
                     <div>
                         <label className="font-bold">Last name</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>
+                        <input type="text" value={lastName} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>
                         <label className="font-bold">phone</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/> 
+                        <input type="text" value={phone} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/> 
                          <label className="font-bold">Date of birth</label>
-                        <input type="text" className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>                          
+                        <input type="date" value={date} className="w-full p-2 outline-none rounded-md mb-2 bg-white"/>                          
                     </div>
                   </div>
                   <button type="submit" className="card w-[20%] p-2 mt-3 ml-[40%] rounded-sm shadow-md font-bold cursor-pointer">Edit</button>
