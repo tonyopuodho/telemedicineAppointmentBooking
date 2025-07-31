c6633c7eb1997bdf3c7346976b4a127835a1fa4d
TONY OCHIENG <tonyopuodho@gmail.com>
2025-07-31 12:09:19 +0300

    implementing count functionalities

--- frontend/components/dashboard/home.jsx
+++ frontend/components/dashboard/home.jsx
@@ -1,6 +1,37 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const [countDoctor,setCountDoctor] = useState(0)
+    const [countPatient,setCountPatient] = useState(0)
+    const [countFeedBack,setCountFeedback] = useState(0)
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/countDoctor')
+        .then(result => {
+            if (result.data.status) {
+                setCountDoctor(result.data.Result[0].total)
+            }
+        })
+        .catch(error => console.log(error))
+
+        axios.get('http://localhost:3000/action/countFeedback')
+        .then(result => {
+            if(result.data.status) {
+                setCountFeedback(result.data.Result[0].total)
+            }
+        })
+        .catch(error => console.log(error))
+
+         axios.get('http://localhost:3000/auth/countPatient')
+        .then(result => {
+            if(result.data.status) {
+                setCountPatient(result.data.Result[0].total)
+            }
+        })
+        .catch(error => console.log(error))
+
+    },[])
+
     return (
         <div className="p-6">
              <div className="p-6 grid md:grid-cols-3 gap-12 grid-cols-1">
@@ -8,21 +39,21 @@ const Home = () => {
                     <h3 className="text-center font-bold text-xl mb-2">Doctors</h3>
                     <hr />
                     <div className="flex gap-65 mt-2 font-extrabold">
-                        <h3>Total</h3> <span>0</span>
+                        <h3>Total</h3> <span>{countDoctor}</span>
                     </div>
                 </div>
                 <div className="p-4 rounded-sm shadow-md card">
                     <h3 className="text-center font-bold text-xl mb-2">Patients</h3>
                     <hr />
                     <div className="flex gap-65 mt-2 font-extrabold">
-                        <h3>Total</h3> <span>0</span>
+                        <h3>Total</h3> <span>{countPatient}</span>
                     </div>
                 </div>
                    <div className="p-4 rounded-sm shadow-md card">
                     <h3 className="text-center font-bold text-xl mb-2">Feedback</h3>
                     <hr />
                     <div className="flex gap-65 mt-2 font-extrabold">
-                        <h3>Total</h3> <span>0</span>
+                        <h3>Total</h3> <span>{countFeedBack}</span>
                     </div>
                 </div>              
              </div>
--- frontend/components/doctors/manageDoctors.jsx
+++ frontend/components/doctors/manageDoctors.jsx
@@ -1,13 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 const ManageDoctors = () => {
+    const [doctor, setDoctor] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/doctors')
+        .then(result => {
+            if (result.data.status) {
+                setDoctor(result.data.Result)
+            }
+        })
+        .catch(error => console.log(error))
+    },[])
+    console.log(doctor)
     return(
         <div className="p-6">
             <Link to={"/dashboard/manageDoctors/addDoctor"} className="py-3 px-5 rounded-sm card mt-3 pb-4 cursor-pointer font-bold outline-none shadow-md">Add doctor</Link>
             <div className="p-4 shadow-sm mt-8 cardAp rounded-sm">
                     <table className="w-full">
                         <thead className="text-center">
-                            <th>image</th>
+                            <th className="">image</th>
                             <th>first name</th>
                             <th>last name</th>
                             <th>phone</th>
@@ -16,18 +30,29 @@ const ManageDoctors = () => {
                             <th>action</th>
                         </thead>
                         <tbody>
-                            <tr className="text-center font-bold ">
-                            <td className="p-2">0</td>
-                            <td>Tony</td>
-                            <td>Ochieng</td>
-                            <td>0794498656</td>
-                            <td>pediatrics</td>
-                            <td>monday</td>
-                            <td>0</td>
-                            </tr>
+                            {
+                                doctor.map((items,index) => (
+                                <tr className="text-center font-bold" key={index}>
+                                    <td className="p-2 w-[15%]"> 
+                                        <img src={`http://localhost:3000/images/` + items.image} alt=""  className=" h-30 w-30"/>
+                                    </td>
+                                    <td>{items.firstName}</td>
+                                    <td>{items.lastName}</td>
+                                    <td>{items.phone}</td>
+                                    <td>{items.specialization}</td>
+                                    <td>{items.schedule}</td>
+                                    <td>
+                                        <button className="p-2 card rounded-sm cursor-pointer outline-none">Edit</button>
+                                        <button className="p-2 card ml-4 rounded-sm cursor-pointer outline-none">Delete</button>
+                                    </td>
+                                </tr>  
+                                ))
+                            }
+                          
                         </tbody>
-                    </table>                
-            </div>
+                    </table>   
+                           
+            </div>  
         </div>
     )
 }
